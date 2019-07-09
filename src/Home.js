9bfa59d38cc8fe09058658471c40a6a299240202
Parth <apple@MBPs-MacBook-Pro.local>
2019-07-09 16:48:10 +0530

    subscriptions for characteristics added

--- src/Home.js
+++ src/Home.js
@@ -1,44 +1,22 @@
 import React, { Component } from "react";
 import { View, Text, PermissionsAndroid } from "react-native";
 import { BleManager } from "react-native-ble-plx";
+import { Observable, pipe } from "rxjs";
+import evaluate from "./SwingLogic";
 
 class Home extends Component {
   constructor() {
     super();
+    this.state = {
+      one: [],
+      two: [],
+      three: [],
+      final: [],
+      device: ""
+    };
     this.manager = new BleManager();
+    this.CHARACTERISTIC_ARRAY = [];
   }
-  discoveredDevices=[]
-  scanAndConnect() {
-    this.manager.startDeviceScan(null, null, (error, device) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-    //   console.log("scanning", device);
-      // Check if it is a device you are looking for based on advertisement data
-      // or other criteria.
-      if (this.discoveredDevices.includes(device.id)==false) {
-        this.discoveredDevices.push(device.id)
-        console.log(device)
-        console.log(this.discoveredDevices)
-
-        // device
-        //   .connect()
-        //   .then(device => {
-        //     return device.discoverAllServicesAndCharacteristics();
-        //   })
-        //   .then(device => {
-        //     // Do work on device with services and characteristics
-        //     console.log("connected device", device)
-        //   })
-        //   .catch(error => {
-        //     // Handle errors
-        //     console.log(error, "error on connection")
-        //   });
-      }
-    });
-  }
-
   componentWillMount() {
     const subscription = this.manager.onStateChange(state => {
       console.log(state);
@@ -49,8 +27,153 @@ class Home extends Component {
     }, true);
   }
 
+
+
+  characteristicOne = () => {
+    return new Observable(observer => {
+      this.manager.monitorCharacteristicForDevice(
+        this.state.device.id,
+        "4fafc201-1fb5-459e-8fcc-c5c9c331914b",
+        "beb5484e-36e1-4688-b7f5-ea07361b26a8",
+        (error, char) => {
+          observer.next(char.value);
+        }
+      );
+    });
+  };
+
+  characteristicTwo = () => {
+    return new Observable(observer => {
+      this.manager.monitorCharacteristicForDevice(
+        this.state.device.id,
+        "4fafc201-1fb5-459e-8fcc-c5c9c331914b",
+        "beb5485e-36e1-4688-b7f5-ea07361b26a8",
+        (error, char) => {
+          observer.next(char.value);
+        }
+      );
+    });
+  };
+
+  characteristicThree = () => {
+    return new Observable(observer => {
+      this.manager.monitorCharacteristicForDevice(
+        this.state.device.id,
+        "4fafc201-1fb5-459e-8fcc-c5c9c331914b",
+        "beb5486e-36e1-4688-b7f5-ea07361b26a8",
+        (error, char) => {
+          observer.next(char.value);
+        }
+      );
+    });
+  };
+
+  async scanAndConnect() {
+    await this.manager.startDeviceScan(null, null, async (error, device) => {
+      if (error) {
+        // Handle error (scanning will be stopped automatically)
+        console.log(error);
+
+        return;
+      }
+      console.log("Scanning...");
+      console.log(device);
+      if (device.name == "ANTON") {
+        this.setState({ device });
+
+        await this.manager.stopDeviceScan();
+        console.log(await this.manager.isDeviceConnected(this.state.device.id));
+
+        // if(await this.manager.isDeviceConnected(this.state.device.id) == true){
+        //   console.log("cancelling");
+
+        //   await this.manager.cancelDeviceConnection(this.state.device.id);
+        // }
+        await this.manager.connectToDevice(this.state.device.id);
+        console.log(await this.manager.isDeviceConnected(this.state.device.id));
+
+        const deviceConnected = await device.isConnected();
+        console.log("=>", deviceConnected);
+        await device.discoverAllServicesAndCharacteristics();
+
+        // await Promise.all([
+        //   this.manager.monitorCharacteristicForDevice(
+        //     device.id,
+        //     "4fafc201-1fb5-459e-8fcc-c5c9c331914b",
+        //     "beb5484e-36e1-4688-b7f5-ea07361b26a8",
+        //     (error, char) => {
+        //       console.log("1", atob(char.value));
+        //       this.setState({ one: atob(char.value) });
+        //     }
+        //   ),
+        //   this.manager.monitorCharacteristicForDevice(
+        //     device.id,
+        //     "4fafc201-1fb5-459e-8fcc-c5c9c331914b",
+        //     "beb5485e-36e1-4688-b7f5-ea07361b26a8",
+        //     (error, char) => {
+        //       this.setState({ two: atob(char.value) });
+        //     }
+        //   ),
+        //   this.manager.monitorCharacteristicForDevice(
+        //     device.id,
+        //     "4fafc201-1fb5-459e-8fcc-c5c9c331914b",
+        //     "beb5486e-36e1-4688-b7f5-ea07361b26a8",
+        //     (error, char) => {
+        //       this.setState({ three: atob(char.value) });
+        //     }
+        //   )
+        // ]);
+
+        this.characteristicOne().subscribe(char => {
+          let charOneArray = atob(char)
+            .trim()
+            .split(",")
+            .map(ite => {
+              return ite.trim();
+            });
+          console.log(this.CHARACTERISTIC_ARRAY)
+          this.setState({ one: charOneArray });
+        });
+
+        this.characteristicTwo().subscribe(char => {
+          let charTwoArray = atob(char)
+            .trim()
+            .split(",")
+            .map(ite => {
+              return ite.trim();
+            });
+          this.setState({ two: charTwoArray });
+        });
+
+        this.characteristicThree().subscribe(char => {
+          let charThreeArray = atob(char)
+            .trim()
+            .split(",")
+            .map(ite => {
+              return ite.trim();
+            });
+          this.setState({ three: charThreeArray });
+        });
+
+      }
+
+      if (error) {
+        console.log(error.message);
+        return;
+      }
+    });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.device != nextState.device) {
+      return true;
+    }
+    return false;
+  }
+
   render() {
-    console.log("discovered",this.discoveredDevices);
+    console.log("render");
+
     return (
       <View>
         <Text>Home</Text>
---
+++ src/SwingLogic.js
@@ -0,0 +1,72 @@
+let x = 0, y = 0, z = 0, count1 = 0, count2 = 0, backlift_angle = 0, follow_through_angle = 0, wagon, velocity = 0, temp = 0;
+let Y = [], X = [], Z = [], A_MAG = [], TIME = [], negative_to_positve = [], positive_to_negative = [], strikes = [], swings = [];
+let swing = {};
+
+export default function evaluate(x, y, z, ax, ay, az) {
+    // console.log("inside eval calculating swing....")
+
+    Y.push(y);
+    X.push(x);
+    Z.push(z);
+    TIME.push(Date.now());
+    
+    let a = calculate_accelerartion(ax, ay, az);
+    A_MAG.push(a);
+    if (Y[Y.length - 2] > 0 && y < 0) {
+
+
+
+        positive_to_negative.push(Y.length - 1);
+
+        count1 = count1 + 1;
+    }
+    if (Y[Y.length - 2] < 0 && y >= 0 && count1 == 1) {
+        negative_to_positve.push(Y.length - 1);
+        count2 = count2 + 1;
+    }
+
+    if (count1 == 1 && count2 == 1 && a >= 5 && y >= 0) {
+
+        console.log(Y.length - 1);
+        backlift_angle = find_backlift(positive_to_negative[0], negative_to_positve[0]);
+        console.log(backlift_angle)
+        velocity = Number(calculate_velocity(positive_to_negative[0], negative_to_positve[0]));
+        negative_to_positve = [];
+        positive_to_negative = [];
+        count1 = 0;
+        count2 = 0;
+
+        swing = { "backklift": backlift_angle, "velocity": Math.abs(velocity), "time_of_impact": Date.now(), "current y index": Y.length - 1 };
+
+        console.log("===>",swing);
+
+
+
+
+    }
+    // if(count1==1 && count2== 1 && Y[Y.length-2]  <  Y[Y.length-3]  && Y[Y.length-2] < y){
+    //     console.log("follow through is :")
+    //     console.log()
+    // }
+}
+
+function calculate_velocity(i, t) {
+    let abc = Number(40.5 * (Math.cos(90 - Math.abs(Y[i])) - Math.cos(90 - Y[t])));
+
+    return abc;
+}
+
+function find_backlift(i, t) {
+
+
+    let temp = Y.slice(i, t);
+    let min = Math.min(...temp);
+    let xyz = Math.abs(min) + 90;
+    return xyz;
+
+}
+
+function calculate_accelerartion(x, y, z) {
+
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+}
